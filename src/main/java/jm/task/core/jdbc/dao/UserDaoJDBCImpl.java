bcbd2fa358aef74fe7ceacf39db2ac08732e44f8
Бабулех <dmitriy.lipenchuk.01@inbox.ru>
2022-06-12 16:33:54 +0300

    Task completed

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,133 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private final Connection connection = Util.getConnection();
+    private boolean isCreated = isCreated();
+    private int idCounter = 1;
 
+
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
 
+        if (!isCreated) {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE users(\n" +
+                        "UserID long,\n" +
+                        "Name varchar(255),\n" +
+                        "LastName varchar(255),\n" +
+                        "Age tinyint CHECK (age < 128)\n" +
+                        ");"
+                );
+
+                preparedStatement.executeUpdate();
+                isCreated = true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void dropUsersTable() {
+        if (isCreated) {
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement("DROP TABLE users");
 
+                preparedStatement.executeUpdate();
+                isCreated = false;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("" +
+                    "INSERT INTO users (userid, name, lastname, age) VALUES (?, ?, ?, ?)");
 
+            preparedStatement.setInt(1, idCounter);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, lastName);
+            preparedStatement.setInt(4, age);
+
+            preparedStatement.executeUpdate();
+
+            idCounter++;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "DELETE FROM users where UserID = ?"
+            );
 
+            preparedStatement.setLong(1, id);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String sql = "SELECT * FROM users";
+            ResultSet resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                User currentUser = new User();
+
+                currentUser.setId(resultSet.getLong("UserID"));
+                currentUser.setName(resultSet.getString("Name"));
+                currentUser.setLastName(resultSet.getString("LastName"));
+                currentUser.setAge(resultSet.getByte("Age"));
+
+                users.add(currentUser);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
     }
 
     public void cleanUsersTable() {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("TRUNCATE TABLE users");
 
+            preparedStatement.executeUpdate();
+
+            idCounter = 1;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isCreated() {
+        try {
+            DatabaseMetaData metaData = connection.getMetaData();
+            ResultSet resultSet = metaData.getTables(null, null, "users", new String[]{"TABLE"});
+
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    static private Connection connection = null;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/kata_schema";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234567890";
+
+    private Util() {
+
+    }
+
+    public static Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        }
+
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
