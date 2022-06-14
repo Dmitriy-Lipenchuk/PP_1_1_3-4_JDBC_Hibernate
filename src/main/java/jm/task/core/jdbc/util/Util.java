a8b8b50536a07448f8c44fe4b501b9f10ff36cf8
Бабулех <dmitriy.lipenchuk.01@inbox.ru>
2022-06-14 22:06:34 +0300

    Added closeConnection method

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,11 +1,20 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    static private Connection connection = null;
+    static private Connection connection;
+    static private Session session;
 
     private static final String URL = "jdbc:mysql://localhost:3306/kata_schema";
     private static final String USERNAME = "root";
@@ -16,17 +25,22 @@ public class Util {
     }
 
     public static Connection getConnection() {
-        if (connection != null) {
-            return connection;
-        }
-
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
 
         return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
