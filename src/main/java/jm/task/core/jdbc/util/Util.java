8ce9daead4202d7497bcac1d3145eed1aab6197d
Бабулех <dmitriy.lipenchuk.01@inbox.ru>
2022-06-15 00:06:28 +0300

    Added getSessionFactory method

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -14,7 +14,7 @@ import java.util.Properties;
 
 public class Util {
     static private Connection connection;
-    static private Session session;
+    static private SessionFactory sessionFactory;
 
     private static final String URL = "jdbc:mysql://localhost:3306/kata_schema";
     private static final String USERNAME = "root";
@@ -38,9 +38,47 @@ public class Util {
 
     public static void closeConnection() {
         try {
-            connection.close();
+            if (connection != null) {
+                connection.close();
+            }
+
+            if (sessionFactory != null) {
+                sessionFactory.close();
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            Configuration configuration = new Configuration();
+
+            Properties settings = new Properties();
+            settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+            settings.put(Environment.URL, URL);
+            settings.put(Environment.USER, USERNAME);
+            settings.put(Environment.PASS, PASSWORD);
+            settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+            settings.put(Environment.SHOW_SQL, "true");
+
+            settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+            settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+            configuration.setProperties(settings);
+
+            configuration.addAnnotatedClass(User.class);
+
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+
+            sessionFactory = configuration.buildSessionFactory(builder.build());
+
+        }
+
+        return sessionFactory;
     }
 }
