f72152bb600e525b9690fe5e8a7626add9630e4b
Бабулех <dmitriy.lipenchuk.01@inbox.ru>
2022-06-14 22:00:28 +0300

    Changed classes declaration by interfaces declaration

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,13 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
 
@@ -21,5 +23,7 @@ public class Main {
         userService.cleanUsersTable();
 
         userService.dropUsersTable();
+
+        Util.closeConnection();
     }
 }
